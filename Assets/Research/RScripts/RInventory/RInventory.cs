eeae464f86a3686e2d0dddb02f2316fc3dd88594
hypochoco <90983535+hypochoco@users.noreply.github.com>
2022-08-16 00:55:09 -0400

    intermediate inventory

--- Assets/Research/RScripts/RInventory/RInventory.cs
+++ Assets/Research/RScripts/RInventory/RInventory.cs
@@ -5,6 +5,46 @@ using UnityEngine;
 // RInventory
 public class RInventory {
 
-    
+    #region RInventory Variables
+
+    // RInventory Variables
+    private Dictionary<int, RItem> _inventory;
+
+    #endregion
+
+    #region Constructor
+
+    // Constructor
+    public RInventory() {
+        _inventory = new Dictionary<int, RItem>();    
+    }
+
+    #endregion
+
+    #region RInventory Functions
+
+    // Find
+    public RItem Find(int ID) {
+        try {
+            return _inventory[ID];
+        } catch (KeyNotFoundException) {
+            return null;
+        }
+    }
+
+    // Add Item
+    public virtual void Add(RItem item) {
+        _inventory[item.ID] = item;
+    }
+
+    // Remove Item
+    public virtual RItem Remove(int ID) {
+        RItem item = this.Find(ID);
+        if (item != null)
+            _inventory.Remove(item.ID);
+        return item;
+    }
+
+    #endregion
 
 }
---
+++ Assets/Research/RScripts/RInventory/RPlayerInventory.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// RPlayerInventory
+public class RPlayerInventory : RInventory {
+
+    #region RPlayerInventory Variables
+
+    // RPlayerInventory Variables
+    private List<Transform> _tList;
+
+    #endregion
+
+    #region Constructor
+
+    // Constructor
+    public RPlayerInventory() {
+        _tList = new List<Transform>();
+    }
+
+    #endregion
+
+    #region RPlayerInventory Functions
+
+    // 
+
+    #endregion
+
+
+}
